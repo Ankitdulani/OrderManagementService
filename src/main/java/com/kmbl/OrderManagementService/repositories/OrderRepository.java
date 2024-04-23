7f8fac9253360693504a3227307671eba2c85359
aakankshak7777 <167679345+aakankshak7777@users.noreply.github.com>
2024-04-23 14:35:48 +0530

    OrderService added

--- src/main/java/com/kmbl/OrderManagementService/repositories/OrderRepository.java
+++ src/main/java/com/kmbl/OrderManagementService/repositories/OrderRepository.java
@@ -2,8 +2,12 @@ package com.kmbl.OrderManagementService.repositories;
 
 
 import com.kmbl.OrderManagementService.models.Order;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@EnableScan
 public interface  OrderRepository extends CrudRepository<Order,String> {
 
 
---
+++ src/main/java/com/kmbl/OrderManagementService/services/OrderService.java
@@ -0,0 +1,56 @@
+package com.kmbl.OrderManagementService.services;
+
+import com.kmbl.OrderManagementService.models.Order;
+import com.kmbl.OrderManagementService.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        orderRepository = orderRepository;
+    }
+
+
+
+    public void createOrder(Order order) {
+       orderRepository.save(order);
+
+    }
+
+
+    public void updateOrder(String orderID,Order order) {
+        Order existingOrder = orderRepository.findById(orderID).orElse(null);
+        if(existingOrder == null){
+
+            return ;
+        }
+
+        existingOrder=order;
+        orderRepository.save(existingOrder);
+
+    }
+
+
+    public void deleteOrder(String orderID) {
+        orderRepository.deleteById(orderID);
+
+    }
+
+
+    public Order getOrder(String orderID) {
+        return orderRepository.findById(orderID).orElse(null);
+    }
+
+
+    public List<Order> getAllOrders() {
+        return (List<Order>) orderRepository.findAll();
+    }
+
+}
